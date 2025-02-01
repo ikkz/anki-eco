6fde37c8ad9c8b716d1fed6ac3df02b0a553f949
ikkz <ylei.mk@gmail.com>
2025-02-01 15:24:14 +0800

    feat(style-kit): add demo

--- packages/style-kit/tailwind.config.js
+++ packages/style-kit/tailwind.config.js
@@ -1,8 +1,18 @@
+import typography from '@tailwindcss/typography';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{html,js}'],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [typography],
+  darkMode: [
+    'variant',
+    [
+      '@media (prefers-color-scheme: dark) {&}',
+      '&.night-mode',
+      '.night-mode &',
+    ],
+  ],
 };
