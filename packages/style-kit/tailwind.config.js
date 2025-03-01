f397798b31bfa06446154868abb025b66a9cbcfa
ikkz <ylei.mk@gmail.com>
2025-03-01 23:52:52 +0800

    fix: remove center align in mobile

--- packages/style-kit/tailwind.config.js
+++ packages/style-kit/tailwind.config.js
@@ -2,7 +2,6 @@ import typography from '@tailwindcss/typography';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js}'],
   theme: {
     extend: {},
   },
